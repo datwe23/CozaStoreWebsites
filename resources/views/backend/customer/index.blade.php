69d1e276c4f43187efe701927b24f82ed30930dd
ThanhCong <nguyenvanthanhcong23122001@gmail.com>
2022-12-05 18:48:31 +0700

    thay doi h2

--- resources/views/backend/customer/index.blade.php
+++ resources/views/backend/customer/index.blade.php
@@ -22,7 +22,6 @@ Danh sách Customers
 
 {{-- Thay thế nội dung vào Placeholder `content` của view `backend.layouts.master` --}}
 @section('content')
-<h2> test </h2>
 </div>
 <body class="g-sidenav-show   bg-gray-100">
     <div class="min-height-300 bg-primary position-absolute w-100"></div>
